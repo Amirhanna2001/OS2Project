ced83f3d8bc2dd48cc7b7b3cd2db0f8cbe7f6336
Amirhanna2001 <amirhanna2001@gmail.com>
2022-12-17 16:57:39 +0200

    Modification

--- ReaderWriterProblemSolution/src/readerwriterproblemsolution/ReaderWriter.java
+++ ReaderWriterProblemSolution/src/readerwriterproblemsolution/ReaderWriter.java
@@ -4,21 +4,20 @@ package readerwriterproblemsolution;
  * @author amirh
  */
 class ReaderWriter {
-    boolean lock = false;
+    boolean read = false;
     int readerCount = 0;
     boolean writer=false;
 
-    public void readerEnter() throws InterruptedException {
+    public void reader() throws InterruptedException {
 
-         while(true) {
             synchronized (this) {
-                while (lock && readerCount == 0) {
+                while (writer) {
                     System.out.println("Reader From " + Thread.currentThread().getName()+" cannot read... Becase another Writer now writing");
                     wait();
                 }
                 
                 ++readerCount;
-                lock = true;
+                read = true;
                 
                 //Sleep For 3 seconds
                 System.out.println("Reader " + readerCount + " from " + Thread.currentThread().getName()+" is reading...");
@@ -26,34 +25,37 @@ class ReaderWriter {
                 System.out.println("Reader " + readerCount + " from " + Thread.currentThread().getName()+" is reading...");
                 Thread.sleep(1000);
                 System.out.println("Reader " + readerCount + " from " + Thread.currentThread().getName()+" is reading...");
-                Thread.sleep(1000);            
-            }
-        }
-    }
-
-    public void readerExit() throws InterruptedException {
-        while (true) {
-            synchronized (this) {
-                while (readerCount != 0) {
-                    System.out.println("Reader " + readerCount + " form " + Thread.currentThread().getName()+" is now exiting...");
-                    readerCount--;
-                }
-                lock = false;
+                Thread.sleep(1000);      
+                
+                read = false;
                 notifyAll();
-//                Thread.sleep(1000);
-
+                --readerCount;
             }
-        }
     }
 
-    public void writerEnter() throws InterruptedException {
-        while (true) {
+    
+    
+//    public void readerExit() throws InterruptedException {
+////        while (true) {
+//            synchronized (this) {
+//                while (readerCount != 0) {
+//                    System.out.println("Reader " + readerCount + " form " + Thread.currentThread().getName()+" is now exiting...");
+//                    readerCount--;
+//                }
+//                lock = false;
+//                notifyAll();
+////                Thread.sleep(1000);
+//
+////            }
+//        }
+//    }
+
+    public void writer() throws InterruptedException {
             synchronized (this) {
-                while (lock){
-                    System.out.println("New writer cannot write... Becase another Writer now writing");
+                while (read||writer){
+                    System.out.println("New writer cannot write...");
                     wait();
                 }
-                lock = true;
                 writer=true;
                 //Sleep for 3 seconds
                 System.out.println("Writer " + Thread.currentThread().getName()+" is now writing.....");
@@ -62,25 +64,27 @@ class ReaderWriter {
                 Thread.sleep(1000);
                 System.out.println("Writer " + Thread.currentThread().getName()+" is now writing.....");
                 Thread.sleep(1000);
+                
+                writer = read = false;
+                notify();
 
 
 
             }
-        }
     }
 
-    public void writerExit() throws InterruptedException {
-        while (true) {
-            synchronized (this) {
-                if (writer) {
-                    System.out.println("Writer " + Thread.currentThread().getName()+" leaving now...");
-                    writer=false;
-                    lock = false;
-                    notifyAll();
-//                    Thread.sleep(100);
-                }
-            }
-        }
-
-    }
+//    public void writerExit() throws InterruptedException {
+////        while (true) {
+//            synchronized (this) {
+//                if (writer) {
+//                    System.out.println("Writer " + Thread.currentThread().getName()+" leaving now...");
+//                    writer=false;
+//                    lock = false;
+//                    notifyAll();
+////                    Thread.sleep(100);
+//                }
+//            }
+////        }
+//
+//    }
 }
--- ReaderWriterProblemSolution/src/readerwriterproblemsolution/ReaderWriterProblemSolution.java
+++ ReaderWriterProblemSolution/src/readerwriterproblemsolution/ReaderWriterProblemSolution.java
@@ -18,10 +18,9 @@ public class ReaderWriterProblemSolution {
         for (int i = 0; i < numberOfThreads; i++) {
             threads[i] = new Thread(() -> { //lamdba  
                 try {
-                rw.readerEnter();
-                rw.writerEnter();
-                rw.readerExit();
-                rw.writerExit();
+                rw.reader();
+                rw.writer();
+                
             } catch (InterruptedException e) {
                 e.printStackTrace(); //for print if any exception happened 
             }
@@ -30,92 +29,10 @@ public class ReaderWriterProblemSolution {
         
         for (int i = 0; i < threads.length; i++){
              threads[i].start() ;
-//             threads[i].join();
+             threads[i].join();
         } 
            
-//        
-//        
-//        for (int i = 0; i < threads.length; i++) 
-//            threads[i].join();
-//            
-        
-//        Thread t1= new Thread(() -> {//using lamdba exp
-//            try {
-//                rw.readerEnter();
-//                rw.writerEnter();
-//                rw.readerExit();
-//                rw.writerExit();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();//for print if any exception happened 
-//            }
-//        });
-//        
-//        Thread t2 =new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    rw.readerEnter();
-//                    rw.writerEnter();
-//                    rw.readerExit();
-//                    rw.writerExit();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        Thread t3= new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    rw.readerEnter();
-//                rw.writerEnter();
-//                rw.readerExit();
-//                rw.writerExit();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        Thread t4= new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    rw.readerEnter();
-//                rw.writerEnter();
-//                rw.readerExit();
-//                rw.writerExit();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        
-//        
-//        Thread t5= new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    rw.readerEnter();
-//                rw.writerEnter();
-//                rw.readerExit();
-//                rw.writerExit();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
 
-//        t1.start();
-//        t2.start();
-//        t3.start();
-//        t4.start();
-//        t5.start();
-//        t1.join();
-//        t3.join();
-//        t2.join();
-//        t4.join();
-//        t5.join();
-        //boinded !!!!
     }
 
     }
